5dc255572a362e7326e46aabae313adc4df01e3c
Gregory Saumier-Finch <gregory@culturecreates.com>
2022-02-15 16:20:55 -0500

    Update readme

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # capacoa-artsdata-usermeta
+
+This is a Wordpress plugin that adds additional fields of User metadata to the WP REST User API so CAPACOA member organizations can be uploaded to Artsdata.
+
+This plug-is for CAPACOA only.
+
+Here is an example call to get a list of 50 users:
+GET /wp-json/wp/v2/users?per_page=50
+
+Here is an example JSON output for user id: "25" in the test environment http://test.capacoa.ca
+  "usermeta": {
+      "operating_name1": "The Test Arts Centre",
+      "operating_name1_language": "en",
+      "operating_name2": "Le Centre des arts bidon",
+      "operating_name2_language": "fr",
+      "legal_name": "THIS IS NOT REAL INC.",
+      "legal_name_language": "en",
+      "also_know_as": "TTAC",
+      "business_number": "123456789",
+      "legal_form": "Municipal corporation",
+      "charitable_status": "Not a charitable organization",
+      "primary_activity": "Presenting/promoting live performances",
+      "discipline_genre": [
+        "Circus"
+      ],
+      "presenting_format": "",
+      "website_official": "http://nowhere.ca",
+      "events_page": "http://nowhere.ca/events",
+      "wikidata_id": "Q20390140293",
+      "venue_name": "Nowhere Theatre",
+      "venue_relationship": "Manager/Operator",
+      "wikidata_id_venue": "Q200917923",
+      "venue_other_name": "",
+      "venue_other_relationship": "",
+      "wikidata_id_venue_other": "",
+      "terms_conditions_v1_0": "agree and disclose (v1.0)",
+      "pmpro_bcountry": "CA",
+      "pmpro_bcity": "Gatineau",
+      "pmpro_bstate": "QC"
+    }
---
+++ capacoa-artsdata-usermeta.php
@@ -0,0 +1,65 @@
+<?php
+
+/*
+Plugin Name: CAPACOA Artsdata User Meta
+Version: 1.0
+Description: This is a Wordpress plugin that enables WP meta data so CAPACOA member organizations can be uploaded to Artsdata.
+Author: Culture Creates
+Author URI: https://culturecreates.com/
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: artsdata
+*/
+
+/** Unlock users that have no WP posts **/
+add_filter('rest_user_query', 'remove_has_published_posts_from_api_user_query', 10, 2);
+function remove_has_published_posts_from_api_user_query($prepared_args, $request)
+{
+	unset($prepared_args['has_published_posts']);
+
+	return $prepared_args;
+}
+
+/** Add metadata fields to WP REST Users API **/
+function get_user_meta_for_restapi($user, $field_name, $request) {
+	  $userObj = get_userdata( $user['id'] );
+    return array (
+		'operating_name1' => $userObj->operating_name1,
+		'operating_name1_language' => $userObj->operating_name1_language,
+		'operating_name2' => $userObj->operating_name2,
+		'operating_name2_language' => $userObj->operating_name2_language,
+		'legal_name' => $userObj->legal_name,
+		'legal_name_language' => $userObj->legal_name_language,
+    'also_know_as' => $userObj->also_known_as,
+		'business_number' => $userObj->business_number,
+		'legal_form' => $userObj->legal_form,
+		'charitable_status' => $userObj->charitable_status,
+    'primary_activity' => $userObj->primary_activity,
+		'discipline_genre' => $userObj->discipline_genre,
+    'presenting_format' => $userObj->presenting_format,
+		'website_official' => $userObj->website_official,
+    'events_page' => $userObj->events_page,
+    'wikidata_id' => $userObj->wikidata_id,
+    'venue_name' => $userObj->venue_name,
+    'venue_relationship' => $userObj->venue_relationship,
+    'wikidata_id_venue' => $userObj->wikidata_id_venue,
+    'venue_other_name' => $userObj->venue_other_name,
+    'venue_other_relationship' => $userObj->venue_other_relationship,
+    'wikidata_id_venue_other' => $userObj->wikidata_id_venue_other,
+    'terms_conditions_v1_0' => $userObj->terms_conditions_v1_0,
+    'pmpro_bcountry' => $userObj->pmpro_bcountry,
+    'pmpro_bcity' => $userObj->pmpro_bcity,
+    'pmpro_bstate' => $userObj->pmpro_bstate,
+    );
+}
+
+function create_restapi_user_obj_field() {
+     register_rest_field('user', 'usermeta', array(
+        'get_callback'    => 'get_user_meta_for_restapi',
+        'schema'          => null,
+    ));
+
+}
+add_action('rest_api_init', 'create_restapi_user_obj_field');
+
+?>
