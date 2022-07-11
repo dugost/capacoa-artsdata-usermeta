5e4982e0fd3904841f223e729613165a52437845
Gregory Saumier-Finch <gregory@culturecreates.com>
2022-07-11 16:54:51 -0400

    Bump v1.0.4

--- capacoa-artsdata-usermeta.php
+++ capacoa-artsdata-usermeta.php
@@ -2,7 +2,7 @@
 
 /*
 Plugin Name: CAPACOA Usermeta for Artsdata.ca
-Version: 1.0
+Version: 1.0.4
 Description: This is a Wordpress plugin that enables WP meta data so CAPACOA member organizations can be uploaded to Artsdata.
 Author: Culture Creates
 Author URI: https://culturecreates.com/
